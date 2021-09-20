a933b085f7c4853143fdf8c21758a539f7a6fc9d
bozo.kopic <bozo@kopic.xyz>
2021-09-20 17:04:30 +0200

    read conf from stdin

--- src_py/hat/gateway/main.py
+++ src_py/hat/gateway/main.py
@@ -35,7 +35,12 @@ def main(conf: typing.Optional[Path]):
             conf = (user_conf_dir / 'gateway').with_suffix(suffix)
             if conf.exists():
                 break
-    conf = json.decode_file(conf)
+
+    if conf == Path('-'):
+        conf = json.decode_stream(sys.stdin)
+    else:
+        conf = json.decode_file(conf)
+
     sync_main(conf)
 
 
