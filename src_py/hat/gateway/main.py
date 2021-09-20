51446af2da031b79ad15ba52adbde982a2a3b49e
bozo.kopic <bozo@kopic.xyz>
2021-09-20 14:26:15 +0200

    split main entry point

--- src_py/hat/gateway/main.py
+++ src_py/hat/gateway/main.py
@@ -30,14 +30,19 @@ user_conf_dir: Path = Path(appdirs.user_config_dir('hat'))
                    "(default $XDG_CONFIG_HOME/hat/gateway.{yaml|yml|json})")
 def main(conf: typing.Optional[Path]):
     """Gateway"""
-    aio.init_asyncio()
-
     if not conf:
         for suffix in ('.yaml', '.yml', '.json'):
             conf = (user_conf_dir / 'gateway').with_suffix(suffix)
             if conf.exists():
                 break
     conf = json.decode_file(conf)
+    sync_main(conf)
+
+
+def sync_main(conf: json.Data):
+    """Sync main entry point"""
+    aio.init_asyncio()
+
     common.json_schema_repo.validate('hat-gateway://main.yaml#', conf)
 
     for device_conf in conf['devices']:
