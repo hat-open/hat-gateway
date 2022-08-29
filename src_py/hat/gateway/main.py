ef2b58050fa03ee0e8e1431ab077dc346414d63e
bozo.kopic <bozo@kopic.xyz>
2022-08-29 13:55:04 +0200

    requirements update

--- src_py/hat/gateway/main.py
+++ src_py/hat/gateway/main.py
@@ -57,7 +57,7 @@ def main():
 
 def sync_main(conf: json.Data):
     """Sync main entry point"""
-    loop = aio.init_asyncio()
+    aio.init_asyncio()
 
     common.json_schema_repo.validate('hat-gateway://main.yaml#', conf)
 
@@ -70,7 +70,7 @@ def sync_main(conf: json.Data):
     logging.config.dictConfig(conf['log'])
 
     with contextlib.suppress(asyncio.CancelledError):
-        aio.run_asyncio(async_main(conf), loop=loop)
+        aio.run_asyncio(async_main(conf))
 
 
 async def async_main(conf: json.Data):
