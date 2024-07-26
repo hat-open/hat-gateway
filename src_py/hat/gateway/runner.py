a71726bfb8fcb8cdcc2fa6262455610f5a786b0d
bozo.kopic <bozo@kopic.xyz>
2024-07-26 21:37:15 +0200

    eventer runner - require operational flag

--- src_py/hat/gateway/runner.py
+++ src_py/hat/gateway/runner.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 import asyncio
 import collections
 import logging
@@ -24,7 +25,7 @@ class MainRunner(aio.Resource):
         self._async_group = aio.Group()
         self._eventer_component = None
         self._eventer_client = None
-        self._engine = None
+        self._eventer_runner = None
 
         self.async_group.spawn(self._run)
 
@@ -63,6 +64,7 @@ class MainRunner(aio.Resource):
                 group=monitor_component_conf['gateway_group'],
                 server_group=monitor_component_conf['event_server_group'],
                 runner_cb=self._create_eventer_runner,
+                status_cb=self._on_component_status,
                 events_cb=self._on_component_events,
                 eventer_kwargs={'subscriptions': subscriptions})
             _bind_resource(self.async_group, self._eventer_component)
@@ -77,20 +79,21 @@ class MainRunner(aio.Resource):
                                  eventer_server_conf['port']),
                 client_name=self._conf['name'],
                 subscriptions=subscriptions,
+                status_cb=self._on_client_status,
                 events_cb=self._on_client_events)
             _bind_resource(self.async_group, self._eventer_client)
 
-            self._engine = hat.gateway.engine.Engine(
+            self._eventer_runner = EventerRunner(
                 conf=self._conf,
                 eventer_client=self._eventer_client)
-            _bind_resource(self.async_group, self._engine)
+            _bind_resource(self.async_group, self._eventer_runner)
 
         else:
             raise Exception('invalid configuration')
 
     async def _stop(self):
-        if self._engine and not self._eventer_component:
-            await self._engine.async_close()
+        if self._eventer_runner and not self._eventer_component:
+            await self._eventer_runner.async_close()
 
         if self._eventer_client:
             await self._eventer_client.async_close()
@@ -100,24 +103,114 @@ class MainRunner(aio.Resource):
 
     async def _create_eventer_runner(self, monitor_component, server_data,
                                      eventer_client):
-        self._engine = hat.gateway.engine.Engine(
-            conf=self._conf,
-            eventer_client=eventer_client)
+        self._eventer_runner = EventerRunner(conf=self._conf,
+                                             eventer_client=eventer_client)
 
-        return self._engine
+        return self._eventer_runner
 
-    async def _process_events(self, events):
-        if not self._engine:
+    def _process_status(self, status):
+        if not self._eventer_runner:
             return
 
-        await self._engine.process_events(events)
+        self._eventer_runner.process_status(status)
 
-    async def _on_component_events(self, monitor_component, eventer_client,
+    async def _process_events(self, events):
+        if not self._eventer_runner:
+            return
+
+        await self._eventer_runner.process_events(events)
+
+    def _on_component_status(self, eventer_component, eventer_client, status):
+        self._process_status(status)
+
+    def _on_client_status(self, eventer_client, status):
+        self._process_status(status)
+
+    async def _on_component_events(self, eventer_component, eventer_client,
                                    events):
         await self._process_events(events)
 
     async def _on_client_events(self, eventer_client, events):
         await self._process_events(events)
+
+
+class EventerRunner(aio.Resource):
+
+    def __init__(self,
+                 conf: json.Data,
+                 eventer_client: hat.event.eventer.Client):
+        self._conf = conf
+        self._eventer_client = eventer_client
+        self._engine = None
+        self._status_event = aio.Event()
+        self._async_group = aio.Group()
+
+        self._status_event.set()
+        self.async_group.spawn(self._run)
+
+    @property
+    def async_group(self) -> aio.Group:
+        return self._async_group
+
+    def process_status(self, status: hat.event.common.Status):
+        self._status_event.set()
+
+    async def process_events(self, events: Iterable[hat.event.common.Event]):
+        if not self.is_open or not self._engine or not self._engine.is_open:
+            return
+
+        await self._engine.process_events(events)
+
+    async def _run(self):
+        try:
+            while True:
+                await self._status_event.wait()
+
+                self._status_event.clear()
+                if not self._is_active():
+                    continue
+
+                self._engine = hat.gateway.engine.Engine(
+                    conf=self._conf,
+                    eventer_client=self._eventer_client)
+
+                async with self._async_group.create_subgroup() as subgroup:
+                    engine_closing_task = subgroup.spawn(
+                        self._engine.wait_closing)
+
+                    while True:
+                        if engine_closing_task.done():
+                            return
+
+                        self._status_event.clear()
+                        if not self._is_active():
+                            break
+
+                        status_task = subgroup.spawn(self._status_event.wait)
+
+                        await asyncio.wait(
+                            [engine_closing_task, status_task],
+                            return_when=asyncio.FIRST_COMPLETED)
+
+                await self._engine.async_close()
+
+        except Exception as e:
+            mlog.error("eventer runner loop error: %s", e, exc_info=e)
+
+        finally:
+            self.close()
+
+            if self._engine:
+                await aio.uncancellable(self._engine.async_close())
+
+    def _is_active(self):
+        if not self._eventer_client.is_open:
+            return False
+
+        if self._conf['event_server'].get('require_operational'):
+            return self._eventer_client.status == hat.event.common.Status.OPERATIONAL  # NOQA
+
+        return True
 
 
 def _bind_resource(async_group, resource):
